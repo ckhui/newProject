a7a9365d0137fdfc512d53d50cfebf6711ff50fa
CKHui <ckhanghui@gmail.com>
2018-02-09 16:03:53 +0800

    Complete Login

--- GitColab/ViewController.swift
+++ GitColab/ViewController.swift
@@ -12,14 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        login()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func login() {
+        print("Login Complete")
     }
-
 
 }
 
